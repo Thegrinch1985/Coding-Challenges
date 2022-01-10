aaf28c2543331a448b339232a85e0a3651beb71f
Howie Lynch <howie.lynch@bearingpoint.com>
2022-01-10 08:09:20 +0000

    Update sum-of-two-values.js

--- sum-of-two-values.js
+++ sum-of-two-values.js
@@ -1,3 +1,9 @@
+
+
+// Given an array of integers and a value, determine if
+//  there are any two integers in the array whose sum is 
+// equal to the given value. Return true if the sum exists and return false if it does not.
+
 let findSumOfTwo = function(arrayOfNumbers, target) {
     let foundValues = new Set();
     for (let a in arrayOfNumbers) {
